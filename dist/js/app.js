35e1f5aa09d65484048283adbdb028c1bac5aa8f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-04-08 12:26:06 +0200

    Add Handlebars template to booking tab and implement Booking class with rendering logic

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,57 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePages(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePages(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  activatePages: function (pageId) {
+    const thisApp = this;
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+  initBooking: function () {
+    // const thisApp = this;
+    const widgetBooking = document.querySelector(select.containerOf.booking);
+
+    new Booking(widgetBooking);
+  },
   initMenu() {
     const thisApp = this;
     for (let productData in thisApp.data.products) {
@@ -15,8 +64,10 @@ const app = {
 
   init() {
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
   initData() {
     const thisApp = this;
@@ -33,7 +84,6 @@ const app = {
         thisApp.data.products = parsedResponse;
         thisApp.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
   initCart: function () {
     const thisApp = this;
